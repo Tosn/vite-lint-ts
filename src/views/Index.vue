a74789e3f4b0716a53a783e63589655ee890177b
Tosn <taoserking@163.com>
2021-11-19 16:07:10 +0800

    feat: demo add get html attribute

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
     Index page {{ store.state }}
+    <h3 ref="titleRef" class="test info">This is a title</h3>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useStore } from 'vuex';
+import { ref, nextTick } from 'vue';
 import { key } from '@/store';
 
 const store = useStore(key);
+
+const titleRef = ref(null);
+nextTick(() => {
+  const t: unknown = titleRef.value;
+  const text = (t as HTMLElement).innerHTML;
+  console.log(text);
+});
+
 </script>
 
 <style scoped>
